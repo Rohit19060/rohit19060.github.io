40ed7e51aad4217479f4b2d70d0688ca59ed5d6a
rohit19060 <rohitjain19060@gmail.com>
2023-05-23 17:42:46 +0530

    👑Code Updated

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
 import card from "./card";
 import getRepos from "./get_repos";
 import badge from "./github_opensource";
+import { Repo } from "./types";
 
 let x = document.createElement("div");
-let repos = (await getRepos()).sort((a, b) => {
-  if (a.fork) {
-    return 1;
-  }
-  if (b.fork) {
-    return -1;
-  }
-  return 0;
-});
-repos.forEach((repo) => {
-  x.appendChild(card(repo));
-});
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+let repos: Repo[] = [];
+
+async function updateRepos() {
+  repos = (await getRepos()).sort((a, b) => {
+    if (a.fork) {
+      return 1;
+    }
+    if (b.fork) {
+      return -1;
+    }
+    return 0;
+  });
+}
+updateRepos().then(() => {
+  repos.forEach((repo) => {
+    x.appendChild(card(repo));
+  });
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div class="header"><h1>My Projects</h1></div>
     <div>${badge("https://github.com/Rohit19060/rohit19060.github.io")}</div>
     <div class="text-center">
@@ -23,19 +29,20 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </div>
     <main></main>`;
 
-document.querySelector<HTMLDivElement>("main")!.appendChild(x);
-
-let searchELE = document.getElementById("search") as HTMLInputElement;
-
-searchELE.addEventListener("input", (e) => {
-  let x = document.createElement("div");
-  let search = (e.target as HTMLInputElement).value;
-  let newCardArr = repos.filter((card) =>
-    card.name.toLowerCase().includes(search.toLowerCase())
-  );
-  newCardArr.forEach((repo) => {
-    x.appendChild(card(repo));
-  });
-  document.querySelector<HTMLDivElement>("main")!.innerHTML = "";
   document.querySelector<HTMLDivElement>("main")!.appendChild(x);
+
+  let searchELE = document.getElementById("search") as HTMLInputElement;
+
+  searchELE.addEventListener("input", (e) => {
+    let x = document.createElement("div");
+    let search = (e.target as HTMLInputElement).value;
+    let newCardArr = repos.filter((card) =>
+      card.name.toLowerCase().includes(search.toLowerCase())
+    );
+    newCardArr.forEach((repo) => {
+      x.appendChild(card(repo));
+    });
+    document.querySelector<HTMLDivElement>("main")!.innerHTML = "";
+    document.querySelector<HTMLDivElement>("main")!.appendChild(x);
+  });
 });
