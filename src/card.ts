8f4b159a563b4bd10c4dc2c078dbfda33b025e2e
Rohit19060 <rohitjain19060@gmail.com>
2023-12-11 15:23:21 +0530

    Vite Updated and some UI Fixes

--- src/card.ts
+++ src/card.ts
@@ -1,10 +1,11 @@
 import "./style.css";
 import { Repo } from "./types";
 
-export default function createCard(e: Repo): Element {
+export default function createCard(e: Repo): Element
+{
   let card = document.createElement("a");
   card.href = e.homepage != null && e.homepage != "" && e.homepage != "null" &&
-      e.homepage != "undefined" && e.homepage.includes("https://")
+    e.homepage != "undefined" && e.homepage.includes("https://")
     ? e.homepage
     : e.html_url;
   card.target = "_blank";
@@ -13,7 +14,7 @@ export default function createCard(e: Repo): Element {
   card.innerHTML = `
   <div>
     <h3>${e.name}</h3>
-    <h5>${e.description == null ? "" : e.description}</h5>
+    <h5 class="description">${e.description == null ? "" : e.description}</h5>
 </div>`;
   card.innerHTML += `<div class="upper_card">
     <a href="${e.html_url}" target="_blank" class="btn">Code</a>
